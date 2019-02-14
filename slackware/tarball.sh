8347ffbe6b7a33b2d4ec5a962790a74c2e75648e
Valery Timiriliyev <timiriliyev@gmail.com>
2019-02-14 20:26:38 +1100

    Add config files to SlackBuild.

--- slackware/tarball.sh
+++ slackware/tarball.sh
@@ -5,6 +5,10 @@ VERSION=${VERSION:-0.2.1}
 set -e
 
 mkdir -p ./pkutils-$VERSION
+
 cp -Rv ../src ./pkutils-$VERSION
+cp -Rv ../conf ./pkutils-$VERSION
+
 tar cJfv ./pkutils-$VERSION.tar.bz2 pkutils-$VERSION
+
 rm -rf ./pkutils-$VERSION

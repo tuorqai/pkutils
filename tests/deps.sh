ef7e4e03b9fd846bb5ef2f12a52b5dd832024b3c
Valery Timiriliyev <timiriliyev@gmail.com>
2019-03-14 02:00:26 +1100

    Updated "deps" test.

--- tests/deps.sh
+++ tests/deps.sh
@@ -1,26 +1,56 @@
 #!/bin/sh
+#
+# Copyright (c) 2019 Valery Timiriliyev timiriliyev@gmail.com
+# 
+# This software is provided 'as-is', without any express or implied
+# warranty. In no event will the authors be held liable for any damages
+# arising from the use of this software.
+# 
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+# 
+# 1. The origin of this software must not be misrepresented; you must not
+#    claim that you wrote the original software. If you use this software
+#    in a product, an acknowledgment in the product documentation would be
+#    appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+#    misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+#
 
-rm -rf /tmp/pkutils-test
-mkdir -p /tmp/pkutils-test
-cd /tmp/pkutils-test
+death() {
+  rm -rf $TMP
+}
+trap death EXIT
 
-mkdir -p ./fake/var/lib/pkutils
-mkdir -p ./fake/var/cache/pkutils
-mkdir -p ./fake/etc/pkutils
+TMP=$(mktemp -p /tmp -d pktest.XXXXXX 2>/dev/null)
+if [ $? -ne 0 ]; then
+    TMP="/tmp/pktest.${RANDOM}"
+    mkdir -m 0700 $TMP || exit 255
+fi
 
-cat <<EOF > ./fake/etc/pkutils/pkutils.conf
+export FAKEROOT=$TMP/fake
+export AWKPATH="$(pwd)/../src/awk"
+export PKUTILS_LIBEXEC="$(pwd)/../src/sh"
+
+mkdir -p $FAKEROOT/var/{lib,cache}/pkutils
+mkdir -p $FAKEROOT/etc/pkutils
+
+cat <<EOF > $FAKEROOT/etc/pkutils/pkutils.conf
 wget_args = '--quiet --show-progress'
 use_deps = yes
 EOF
 
-cat <<EOF > ./test.awk
+cat <<EOF > $TMP/test.awk
+@include "pkutils.foundation.awk"
 @include "pkutils.deps.awk"
 
 BEGIN {
-    pk_setup_dirs("./fake");
+    pk_setup_dirs(ENVIRON["FAKEROOT"]);
     db_rebuild();
     p = db_get_by_name(PACKAGENAME);
-    pk_make_dependency_list(p, dlist);
+    add_to_dependency_list(p, dlist, 0);
     for (i = 1; i <= dlist["length"]; i++) {
         printf DB[dlist[i]]["name"] " ";
     }
@@ -30,117 +60,117 @@ EOF
 
 # Sluceai 1.: net zavisimostei
 
-cat <<EOF > ./fake/var/lib/pkutils/index.dat
-test:.:.:app:2.22.1:i586:1:tt:tlz:00000000000000000000000000000000:Sample app:::
+cat <<EOF > $FAKEROOT/var/lib/pkutils/index.dat
+test,.,.,app,2.22.1,i586,1,tt,tlz,00000000000000000000000000000000,"Sample app",,,,,,,
 EOF
 
-gawk -v PACKAGENAME=app -f "./test.awk" > ./result.txt 2> /dev/null
+gawk -v PACKAGENAME=app -f "$TMP/test.awk" > $TMP/result.txt 2> /dev/null
 echo -e "[1]: \c"
-if [ "$(cat ./result.txt)" = "app " ]; then
+if [ "$(cat $TMP/result.txt)" = "app " ]; then
     echo "PASSED!"
 else
     echo "FAILED!"
-    echo "Got \"$(cat ./result.txt)\" instead of \"app \"."
+    echo "Got \"$(cat $TMP/result.txt)\" instead of \"app \"."
     exit 1
 fi
 
 # Sluceai 2.: esth odna zavisimosth
 
-cat <<EOF > ./fake/var/lib/pkutils/index.dat
-test:.:.:app:2.22.1:i586:1:tt:tlz:00000000000000000000000000000000:Sample app:libbeta::
-test:.:.:libbeta:0.68.3:i586:1:tt:tlz:00000000000000000000000000000000:Beta lib:::
+cat <<EOF > $FAKEROOT/var/lib/pkutils/index.dat
+test,.,.,app,2.22.1,i586,1,tt,tlz,00000000000000000000000000000000,"Sample app",libbeta,,,,,,
+test,.,.,libbeta,0.68.3,i586,1,tt,tlz,00000000000000000000000000000000,"Beta lib",,,,,,,
 EOF
 
-gawk -v PACKAGENAME=app -f "./test.awk" > ./result.txt 2> /dev/null
+gawk -v PACKAGENAME=app -f "$TMP/test.awk" > $TMP/result.txt 2> /dev/null
 echo -e "[2]: \c"
-if [ "$(cat ./result.txt)" = "libbeta app " ]; then
+if [ "$(cat $TMP/result.txt)" = "libbeta app " ]; then
     echo "PASSED!"
 else
     echo "FAILED!"
-    echo "Got \"$(cat ./result.txt)\" instead of \"libbeta app \"."
+    echo "Got \"$(cat $TMP/result.txt)\" instead of \"libbeta app \"."
     exit 1
 fi
 
 # Sluceai 3.: esth dve zavisimosti
 
-cat <<EOF > ./fake/var/lib/pkutils/index.dat
-test:.:.:app:2.22.1:i586:1:tt:tlz:00000000000000000000000000000000:Sample app:libbeta,libsuck::
-test:.:.:libbeta:0.68.3:i586:1:tt:tlz:00000000000000000000000000000000:Beta test:::
-test:.:.:libsuck:14.88.5:i586:4:tt:tlz:00000000000000000000000000000000:Suck my lib:::
+cat <<EOF > $FAKEROOT/var/lib/pkutils/index.dat
+test,.,.,app,2.22.1,i586,1,tt,tlz,00000000000000000000000000000000,"Sample app",libbeta libsuck,,,,,,
+test,.,.,libbeta,0.68.3,i586,1,tt,tlz,00000000000000000000000000000000,"Beta test",,,,,,,
+test,.,.,libsuck,14.88.5,i586,4,tt,tlz,00000000000000000000000000000000,"Suck my lib",,,,,,,
 EOF
 
-gawk -v PACKAGENAME=app -f "./test.awk" > ./result.txt 2> /dev/null
+gawk -v PACKAGENAME=app -f "$TMP/test.awk" > $TMP/result.txt 2> /dev/null
 echo -e "[3]: \c"
-if [ "$(cat ./result.txt)" = "libbeta libsuck app " ]; then
+if [ "$(cat $TMP/result.txt)" = "libbeta libsuck app " ]; then
     echo "PASSED!"
 else
     echo "FAILED!"
-    echo "Got \"$(cat ./result.txt)\" instead of \"libbeta libsuck app \"."
+    echo "Got \"$(cat $TMP/result.txt)\" instead of \"libbeta libsuck app \"."
     exit 1
 fi
 
 # Sluceai 4.: cyclicescaya zavisimosth
 
-cat <<EOF > ./fake/var/lib/pkutils/index.dat
-test:.:.:app:2.22.1:i586:1:tt:tlz:00000000000000000000000000000000:Sample app:libbeta,libsuck::
-test:.:.:libbeta:0.68.3:i586:1:tt:tlz:00000000000000000000000000000000:Beta test:::
-test:.:.:libsuck:14.88.5:i586:4:tt:tlz:00000000000000000000000000000000:Suck my lib:app::
+cat <<EOF > $FAKEROOT/var/lib/pkutils/index.dat
+test,.,.,app,2.22.1,i586,1,tt,tlz,00000000000000000000000000000000,"Sample app",libbeta libsuck,,,,,,
+test,.,.,libbeta,0.68.3,i586,1,tt,tlz,00000000000000000000000000000000,"Beta test",,,,,,,
+test,.,.,libsuck,14.88.5,i586,4,tt,tlz,00000000000000000000000000000000,"Suck my lib",app,,,,,,
 EOF
 
-gawk -v PACKAGENAME=app -f "./test.awk" > ./result.txt 2> /dev/null
+gawk -v PACKAGENAME=app -f "$TMP/test.awk" > $TMP/result.txt 2> /dev/null
 echo -e "[4]: \c"
-if [ "$(cat ./result.txt)" = "libbeta libsuck app " ]; then
+if [ "$(cat $TMP/result.txt)" = "libbeta libsuck app " ]; then
     echo "PASSED!"
 else
     echo "FAILED!"
-    echo "Got \"$(cat ./result.txt)\" instead of \"libbeta libsuck app \"."
+    echo "Got \"$(cat $TMP/result.txt)\" instead of \"libbeta libsuck app \"."
     exit 1
 fi
 
 # Sluceai 5.: glubocoe derevo zavisimostei
 
-cat <<EOF > ./fake/var/lib/pkutils/index.dat
-test:.:.:alpha:1.0.0:i586:1:tt:tlz:00000000000000000000000000000000::bravo::
-test:.:.:bravo:2.0.0:i586:2:tt:tlz:00000000000000000000000000000000::charlie::
-test:.:.:charlie:3.0.0:i586:3:tt:tlz:00000000000000000000000000000000::delta::
-test:.:.:delta:4.0.0:i586:4:tt:tlz:00000000000000000000000000000000::echo::
-test:.:.:echo:5.0.0:i586:5:tt:tlz:00000000000000000000000000000000::foxtrot,golf::
-test:.:.:foxtrot:6.0.0:i586:6:tt:tlz:00000000000000000000000000000000::india::
-test:.:.:golf:7.0.0:i586:7:tt:tlz:00000000000000000000000000000000::hotel::
-test:.:.:hotel:8.0.0:i586:8:tt:tlz:00000000000000000000000000000000::::
-test:.:.:india:9.0.0:i586:9:tt:tlz:00000000000000000000000000000000::::
+cat <<EOF > $FAKEROOT/var/lib/pkutils/index.dat
+test,.,.,alpha,1.0.0,i586,1,tt,tlz,00000000000000000000000000000000,,bravo,,,,,,
+test,.,.,bravo,2.0.0,i586,2,tt,tlz,00000000000000000000000000000000,,charlie,,,,,,
+test,.,.,charlie,3.0.0,i586,3,tt,tlz,00000000000000000000000000000000,,delta,,,,,,
+test,.,.,delta,4.0.0,i586,4,tt,tlz,00000000000000000000000000000000,,echo,,,,,,
+test,.,.,echo,5.0.0,i586,5,tt,tlz,00000000000000000000000000000000,,foxtrot golf,,,,,,
+test,.,.,foxtrot,6.0.0,i586,6,tt,tlz,00000000000000000000000000000000,,india,,,,,,
+test,.,.,golf,7.0.0,i586,7,tt,tlz,00000000000000000000000000000000,,hotel,,,,,,
+test,.,.,hotel,8.0.0,i586,8,tt,tlz,00000000000000000000000000000000,,,,,,,,
+test,.,.,india,9.0.0,i586,9,tt,tlz,00000000000000000000000000000000,,,,,,,,
 EOF
 
-gawk -v PACKAGENAME=alpha -f "./test.awk" > ./result.txt 2> /dev/null
+gawk -v PACKAGENAME=alpha -f "$TMP/test.awk" > $TMP/result.txt 2> /dev/null
 echo -e "[5]: \c"
-if [ "$(cat ./result.txt)" = "india foxtrot hotel golf echo delta charlie bravo alpha " ]; then
+if [ "$(cat $TMP/result.txt)" = "india foxtrot hotel golf echo delta charlie bravo alpha " ]; then
     echo "PASSED!"
 else
     echo "FAILED!"
-    echo "Got \"$(cat ./result.txt)\" instead of \"india foxtrot hotel golf echo delta charlie bravo alpha \"."
+    echo "Got \"$(cat $TMP/result.txt)\" instead of \"india foxtrot hotel golf echo delta charlie bravo alpha \"."
     exit 1
 fi
 
 # Sluceai 6.: glubocoe derevo zavisimostei s zacyclivaniem
 
-cat <<EOF > ./fake/var/lib/pkutils/index.dat
-test:.:.:alpha:1.0.0:i586:1:tt:tlz:00000000000000000000000000000000::bravo::
-test:.:.:bravo:2.0.0:i586:2:tt:tlz:00000000000000000000000000000000::charlie::
-test:.:.:charlie:3.0.0:i586:3:tt:tlz:00000000000000000000000000000000::delta::
-test:.:.:delta:4.0.0:i586:4:tt:tlz:00000000000000000000000000000000::echo::
-test:.:.:echo:5.0.0:i586:5:tt:tlz:00000000000000000000000000000000::foxtrot,golf::
-test:.:.:foxtrot:6.0.0:i586:6:tt:tlz:00000000000000000000000000000000::india::
-test:.:.:golf:7.0.0:i586:7:tt:tlz:00000000000000000000000000000000::hotel::
-test:.:.:hotel:8.0.0:i586:8:tt:tlz:00000000000000000000000000000000::::
-test:.:.:india:9.0.0:i586:9:tt:tlz:00000000000000000000000000000000::bravo::
+cat <<EOF > $FAKEROOT/var/lib/pkutils/index.dat
+test,.,.,alpha,1.0.0,i586,1,tt,tlz,00000000000000000000000000000000,,bravo,,,,,,
+test,.,.,bravo,2.0.0,i586,2,tt,tlz,00000000000000000000000000000000,,charlie,,,,,,
+test,.,.,charlie,3.0.0,i586,3,tt,tlz,00000000000000000000000000000000,,delta,,,,,,
+test,.,.,delta,4.0.0,i586,4,tt,tlz,00000000000000000000000000000000,,echo,,,,,,
+test,.,.,echo,5.0.0,i586,5,tt,tlz,00000000000000000000000000000000,,foxtrot golf,,,,,,
+test,.,.,foxtrot,6.0.0,i586,6,tt,tlz,00000000000000000000000000000000,,india,,,,,,
+test,.,.,golf,7.0.0,i586,7,tt,tlz,00000000000000000000000000000000,,hotel,,,,,,
+test,.,.,hotel,8.0.0,i586,8,tt,tlz,00000000000000000000000000000000,,,,,,,,
+test,.,.,india,9.0.0,i586,9,tt,tlz,00000000000000000000000000000000,,bravo,,,,,,
 EOF
 
-gawk -v PACKAGENAME=alpha -f "./test.awk" > ./result.txt 2> /dev/null
+gawk -v PACKAGENAME=alpha -f "$TMP/test.awk" > $TMP/result.txt 2> /dev/null
 echo -e "[6]: \c"
-if [ "$(cat ./result.txt)" = "india foxtrot hotel golf echo delta charlie bravo alpha " ]; then
+if [ "$(cat $TMP/result.txt)" = "india foxtrot hotel golf echo delta charlie bravo alpha " ]; then
     echo "PASSED!"
 else
     echo "FAILED!"
-    echo "Got \"$(cat ./result.txt)\" instead of \"india foxtrot hotel golf echo delta charlie bravo alpha \"."
+    echo "Got \"$(cat $TMP/result.txt)\" instead of \"india foxtrot hotel golf echo delta charlie bravo alpha \"."
     exit 1
 fi
